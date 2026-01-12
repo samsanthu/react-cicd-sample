1bd1aaf71c93a9b05db9b0d1416b7fb1cef627de
bhaskararao1.m@gmail.com <bhaskararao1.m@gmail.com>
2026-01-12 14:49:40 +0530

    Initial React TSX app

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+import './App.css'
+function App() {
+  return (
+    <div style={{ padding: "40px", textAlign: "center" }}>
+      <h1>React CI/CD Sample 🚀</h1>
+      <p>Project name: react-cicd-sample</p>
+    </div>
+  );
+}
+
+export default App;
